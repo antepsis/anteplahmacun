8eed6ea43c61db2dc8d522f2d8b01380810ace96
dogacogz <dogacogz@yahoo.com>
2016-12-10 19:02:24 +0200

    Hash function added.

--- sympy/crypto/chainedHashTable.py
+++ sympy/crypto/chainedHashTable.py
@@ -1,3 +1,6 @@
+def hash_it(key, tablesize):
+    return sum([ord(c) for c in key]) * 805306457
+
 class chainedHashTable:
 	
 	def __init__(self, capacity = 128):
@@ -6,13 +9,19 @@ class chainedHashTable:
 		self.keys = []
 		self.data = [[] for _ in range(capacity)]
 
-    def addUpdate(self, key, obj):
+    def addUpdate(self, obj):
         
 
     def remove(self, key):
 
 
     def fetch(self, key):
+        def fetch_item(item, _):
+            if item:
+                return item[1]
+            else
+                raise KeyError(key)
+                
 
 
-
+    
