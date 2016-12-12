df91d25e232bfd2d09e7a716b8822dbcba4c6198
dogacogz <dogacogz@yahoo.com>
2016-12-12 13:41:18 +0300

    hash table insertion added.

--- sympy/crypto/chainedHashTable.py
+++ sympy/crypto/chainedHashTable.py
@@ -1,26 +1,41 @@
-def hash_it(key, tablesize):
-    return sum([ord(c) for c in key]) * 805306457
+class ChainedHashTable:
+    def __init__(self, capacity=128):
+        self.capacity = capacity;
+        self.size = 0;
+        self.keys = []
+        self.data = [[] for _ in range(self.capacity)]
 
-class chainedHashTable:
-	
-	def __init__(self, capacity = 128):
-		self.capacity = capacity;
-		self. size = 0;
-		self.keys = []
-		self.data = [[] for _ in range(capacity)]
+
+    def __str__(self):
+        table_status = ""
+        for cells in self.data:
+            table_status += str(cells)
+        return table_status
+
+
+    def __len__(self):
+        length = 0
+        for i in self.data:
+            length += len(i)
+        return length
+
+
+    def hash_it(obj, tablesize):  # gotta take mod through table size
+        return sum([ord(c) for c in obj]) * 805306457
+
 
     def addUpdate(self, obj):
-        
+        hashed = self.hash_it(obj, self.capacity)
+        for i in enumerate(self.data[hashed%self.capacity]):
+            if self.data[i%self.capacity] == self.data[hashed%self.capacity]:
+                print "Insert ERROR. This object already exists in the table."
+            else:
+                self.data.append(obj)
 
     def remove(self, key):
 
-
-    def fetch(self, key):
-        def fetch_item(item, _):
-            if item:
-                return item[1]
-            else
-                raise KeyError(key)
+if __name__ == "__main__":
+    table = ChainedHashTable(64)
                 
 
 
