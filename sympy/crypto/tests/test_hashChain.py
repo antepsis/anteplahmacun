8f8242a580e5465515e913c7ac0ad8db9d04fdc6
dogacogz <dogacogz@yahoo.com>
2016-12-20 20:18:03 +0300

    Explained usage via comments.

--- sympy/crypto/tests/test_hashChain.py
+++ sympy/crypto/tests/test_hashChain.py
@@ -3,6 +3,3 @@ from sympy.crypto.chainedHashTable import ChainedHashTable
 
 if __name__ == "__main__":
 	table = ChainedHashTable(32)
-	table.addChain('sikik', 7)
-	table.printTable()
-	print (table.randomize('sikik'))
