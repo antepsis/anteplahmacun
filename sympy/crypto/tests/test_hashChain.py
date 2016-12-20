1f19e6f3f853993075a1478e6046c194b7dd6a84
dogacogz <dogacogz@yahoo.com>
2016-12-20 20:37:02 +0300

    Test functions added.

--- sympy/crypto/tests/test_hashChain.py
+++ sympy/crypto/tests/test_hashChain.py
@@ -1,5 +1,46 @@
+import hashlib
 import sys
 from sympy.crypto.chainedHashTable import ChainedHashTable
 
+def test_getSize():
+    print ('Going to be filled.')
+
+def test_wipeTable():
+    print ('Going to be filled.')
+
+def test_getHash():
+    print ('Going to be filled.')
+
+def test_getKey():
+    print ('Going to be filled.')
+
+def test_addChain():
+    print ('Going to be filled.')
+
+def test_search():
+    print ('Going to be filled.')
+
+def test_searchHash():
+    print ('Going to be filled.')
+
+def test_randomize():
+    print ('Going to be filled.')
+
+def test_fetchAtLine():
+    print ('Going to be filled.')
+
+def test_controlData():
+    print ('Going to be filled.')
+
+
 if __name__ == "__main__":
-	table = ChainedHashTable(32)
+    test_getSize()
+    test_wipeTable()
+    test_getHash()
+    test_getKey()
+    test_addChain()
+    test_search()
+    test_searchHash()
+    test_randomize()
+    test_fetchAtLine()
+    test_controlData()
