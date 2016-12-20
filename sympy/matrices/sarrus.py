bc3c3ec08c7d4b241c79da136f3cb034ab403e63
alperenboyaci <alperen.boyaci@hotmail.com>
2016-12-21 02:03:00 +0300

    baska bir hata yakalama eklendi

--- sympy/matrices/sarrus.py
+++ sympy/matrices/sarrus.py
@@ -1,11 +1,16 @@
-
+from numbers import Number
+from decimal import Decimal
+from fractions import Fraction
 
 def calc_sarrus_det(matrix):
 #checking for size property is suitable for Sarrus Theorem
   size1 = len(matrix)
   size2 = len(matrix[0])
- # if(size1 != size2 or (size1 != 2 or size1 != 3)):
-    # print("Can not calculate determinant with Sarrus Theorem(Matrix must be 2x2 or 3x3)")
+  for i in range(size1-1):
+    for j in range(size2-1):
+      if not (isinstance(matrix[i][j],Number)):
+         raise ValueError("Matrix should be consist of only NUMBERS!")
+  
   try:  
    if(size1 == size2 and size1 == 2):
       det2x2 = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]) 	
@@ -17,7 +22,7 @@ def calc_sarrus_det(matrix):
      print("Can not calculate determinant with Sarrus Theorem(Matrix must be 2x2 or 3x3)") 
 
 
-m =[[3,-2,1],[5,6,2],[1,0]]
+m =[[3,-2,1],[1,6,2],[1,0,2]]
 
 x = calc_sarrus_det(m)
 
