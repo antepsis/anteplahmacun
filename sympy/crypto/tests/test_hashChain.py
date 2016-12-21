0d815fa32d089f0a7253c1175899de216b21eaa7
dogacogz <dogacogz@yahoo.com>
2016-12-21 17:18:19 +0300

    Some tests added.

--- sympy/crypto/tests/test_hashChain.py
+++ sympy/crypto/tests/test_hashChain.py
@@ -3,16 +3,39 @@ import sys
 from sympy.crypto.chainedHashTable import ChainedHashTable
 
 def test_getSize():
-    print ('Going to be filled.')
+    table = ChainedHashTable(32)
+    table.addChain('test0', 5)
+    table.addChain('test1', 3)
+    assert table.getSize() == 2
 
 def test_wipeTable():
-    print ('Going to be filled.')
+    table = ChainedHashTable(16)
+    table.addChain('test0', 5)
+    table.addChain('test1', 23)
+    table.addChain('test2', 7)
+    table.addChain('test3', 4)
+    size = table.getSize()
+    
+    table.wipeTable()
+    assert size != table.getSize()
+    for i in range(16):
+        assert table.fetchAtLine(i) == 0
 
 def test_getHash():
-    print ('Going to be filled.')
+    table = ChainedHashTable(16)
+    hexHash0 = hashlib.md5('test').hexdigest()
+    hexHash1 = hashlib.md5('').hexdigest()
+    assert hexHash0 == table.getHash('test')
+    assert hexHash1 == table.getHash('')
 
 def test_getKey():
-    print ('Going to be filled.')
+    table = ChainedHashTable(16)
+    hashedValue0 = hashlib.md5('test').hexdigest()
+    hashedValue1 = hashlib.md5('').hexdigest()
+    key0 = table.getKey(hashedValue0) 
+    key1 = table.getKey(hashedValue1)
+    assert key0 >= 0 and key0 <= 16
+    assert key1 >= 0 and key1 <= 16
 
 def test_addChain():
     print ('Going to be filled.')
