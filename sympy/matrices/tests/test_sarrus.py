4607bc11cb6c4451c4dd2af927e5d833346681b5
alperenboyaci <alperen.boyaci@hotmail.com>
2016-12-21 21:47:29 +0300

    final situation

--- sympy/matrices/tests/test_sarrus.py
+++ sympy/matrices/tests/test_sarrus.py
@@ -5,7 +5,18 @@ class test_sarrus():
   def test_calc_sarrus_det():
     s = sarrus()
     m =[[2,1,0],[0,2,0],[2,0,1]]
+    #testing proper value for proper entry
     assert s.calc_sarrus_det(m) == 4
-
+    m1 =[[1,2],[3,4]]
+    assert s.calc_sarrus_det(m1) == -2
+    
+  def test_calc_sarrus_det():
+    s = sarrus()
+    m =[[2,1,0],[0,2,0],[2,0,1]]
+    #testing proper value for proper entry
+    assert s.calc_reverse_matrix(m) == 4
+    m1 =[[1,2],[3,4]]
+    assert s.calc_reverse_matrix(m1) == -2
+     
 
   test_calc_sarrus_det()
