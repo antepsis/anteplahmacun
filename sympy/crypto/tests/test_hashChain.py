5c952dff921f9347d3c40de577b48c774e9ad316
dogacogz <dogacogz@yahoo.com>
2016-12-21 19:11:52 +0300

    Some tests added and fixed data control algorithm's bug.

--- sympy/crypto/tests/test_hashChain.py
+++ sympy/crypto/tests/test_hashChain.py
@@ -92,9 +92,10 @@ def test_controlData():
     hashes.append(hashedValue0)
     table.printTable()
     for i in range(50):
-        hashes.append(hashes[i])
-    print (hashes[1])
-    assert table.controlData(hashes[29]) == True
+        hashes.append(hashlib.md5(hashes[i]).hexdigest())
+    print (hashes[4])
+    print table.searchHash(hashes[4])
+    assert table.controlData(table.randomize('test1')) == True
     assert table.controlData(hashes[45]) == True
     assert table.controlData(hashedValue1) == False
 
